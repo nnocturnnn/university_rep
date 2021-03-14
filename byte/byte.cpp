0186dde14a3ac32a3cf590a3ff19b5dc70c030df
nnocturnnn <vikchehovich@gmail.com>
2021-03-14 21:45:32 +0200

    full b

--- byte/byte.cpp
+++ byte/byte.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 
@@ -6,7 +7,7 @@ int main() {
     int a = 110;
     int b = -78;
     int c = 32;
-    float d = -60.34375;
+    double d = -60.34375;
     int res_int;
     std::string binary_a = std::bitset<8>(a).to_string();
     cout << "A = " << a << " Byte A = " << binary_a << endl;
@@ -26,5 +27,10 @@ int main() {
     std :: bitset <8> sum (b3.to_ulong () + b4.to_ulong());
     res_int = (int)(sum.to_ulong()) * -1;res_int--;
     cout << "Byte B - Byte C = Byte " << sum << " And Int " << res_int << endl;
+    double f_1 = 0.10101 * pow(2,-1);
+    double f_2 = 0.10101 * pow(2, 11);
+    double rez = (d + f_1) * f_2;
+    cout <<  "(D + 0.10101 * 2(-1)) * 0.10101 * 2(11)) = " << rez << endl;
+
 }
 
