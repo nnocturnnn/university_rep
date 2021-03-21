fee8107c60395faa21b2bde56ba89508e5588be9
nnocturnnn <vikchehovich@gmail.com>
2021-03-21 19:03:26 +0200

    doc

--- project_java/first/src/main/java/first/Main.java
+++ project_java/first/src/main/java/first/Main.java
@@ -6,38 +6,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		Parent parent = FXMLLoader.load(this.getClass().getResource("/Main.fxml"));
-		Scene scene = new Scene(parent);
-		primaryStage.show();
-		primaryStage.setScene(scene);
-		primaryStage.setTitle("First");
-		primaryStage.centerOnScreen();
-		primaryStage.resizableProperty();
-	}
-}
-
-//public class Main {
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
 //
-//	public static void main(String[] args) {
-//		LocalDate d=null;
+//public class Main extends Application {
 //
-//		ProgramGuide programGuide = new ProgramGuide(d.now());
-//
-//		programGuide.createTvChannel("1", LocalTime.of(8, 00), "New ");
-//		programGuide.createTvChannel("1", LocalTime.of(8, 00), "Ukraine");
-//		programGuide.createTvChannel("1", LocalTime.of(8, 30), "5C");
-//
-//		programGuide.receiveProgram("1", programGuide.getTvChannel());
+//	@Override
+//	public void start(Stage primaryStage) throws Exception {
+//		Parent parent = FXMLLoader.load(this.getClass().getResource("1.fxml"));
+//		Scene scene = new Scene(parent);
+//		primaryStage.show();
+//		primaryStage.setScene(scene);
+//		primaryStage.setTitle("First");
+//		primaryStage.centerOnScreen();
+//		primaryStage.resizableProperty();
 //	}
-//
 //}
+
+public class Main {
+
+	public static void main(String[] args) {
+		LocalDate d=null;
+
+		ProgramGuide programGuide = new ProgramGuide(d.now());
+
+		programGuide.createTvChannel("1", LocalTime.of(8, 00), "New ");
+		programGuide.createTvChannel("1", LocalTime.of(8, 00), "Ukraine");
+		programGuide.createTvChannel("1", LocalTime.of(8, 30), "5C");
+
+		programGuide.receiveProgram("1", programGuide.getTvChannel());
+	}
+
+}
