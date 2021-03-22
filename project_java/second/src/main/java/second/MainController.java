7a2bc0fc0fa0f6d7056c1b16f787cd0e4cdeaa9c
nnocturnnn <vikchehovich@gmail.com>
2021-03-22 22:31:39 +0200

    pr

--- project_java/second/src/main/java/second/MainController.java
+++ project_java/second/src/main/java/second/MainController.java
@@ -4,19 +4,35 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import java.io.IOException;
 import java.io.*;
+import java.util.Random;
 
-public class MainController {
-    String[] arr = {"3242","3242","32442","213"};
+public class MainController  {
+    String[] arry = {};
     @FXML public Label arr;
+    String stri = "";
     @FXML
-    public void print() {
-        String stri;
-        for (String str : arr) {
-            stri += str + "\n";
+    public void print() throws IOException{ 
+        for (int i = 0; i < 5; i++) {
+            arry[i] = String.valueOf(getRandomNumberInRange(0, 1000000));
+        }
+        for (String str : arry) {
+            stri += str + " - " + Integer.toString(str.length()) + "\n";
         }
         OutputStream os = new FileOutputStream(new File("db.txt"), true);
         os.write(stri.getBytes(), 0, stri.length());
         os.close();
+        arr.setText(stri);
+    }
+
+    private static int getRandomNumberInRange(int min, int max) {
+
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
     }
 }
